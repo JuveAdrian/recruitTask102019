6a2d280efe2ffdd50a4ea7c8537e3cb120f95282
Adrian Grabkowski <adrian.grabkowski@wp.pl>
2019-10-15 22:13:12 +0200

    good save, in next try save all in objects array

--- controllers/users.js
+++ controllers/users.js
@@ -23,16 +23,65 @@ exports.postFormData = (req, res, next) => {
         }
     }).then((data) => {
         let cities = data.results;
+        let citiesArr = [];
         cities.sort((a, b) => (a.measurements[0].value > b.measurements[0].value) ? -1 : 1);
+
+        for(let item of cities)
+        citiesArr.push({ name: item.city, value: item.measurements[0].value} );
+        
+        console.log(citiesArr);
+        
         return cities;
     }).then((apidata) => {
         
+            // ----------------------
             // usunac duplikaty miast
+            // ----------------------
+        
 
+        for(let i=0; i<11; i++) {
+
+            let city_query;
+
+            if (country.selectCountry === "DE" || country.selectCountry == "ES") {
+                city_query = apidata[i].location;
+            } else {
+                city_query = apidata[i].city;
+            }
+            
+            city_query.replace(" ", "_");
+            
+
+            const urlWikiApi = `https://pl.wikipedia.org/w/api.php?action=opensearch&format=json&search=${city_query}`;
+            console.log(urlWikiApi);
+            fetch(urlWikiApi).then((response) => {
+                if (response.status === 200) {
+                    return response.json()
+                } else {
+                    throw new Error('Unable to fetch the data');
+                }
+            }).then((data, err) => {
+                if(err) {
+                    console.log(err);
+                } else {
+                    console.log(`OPIS - ${data[2][0]}`);
+                }
+                
+                
+                
+            }).catch((err) => {
+                if(err) {
+                    console.log('Blad pobierania opisu');
+                }
+            });
+            
+        }
         res.render('results', {
             status: 'udalosiewyswietlic',
             country: data.selectCountry,
-            data: apidata
+            data: apidata,
+            country: country
         });
+        
     });
 };
--- models/country.js
+++ models/country.js
@@ -15,31 +15,4 @@ module.exports = class Country {
         return country;
     }
     
-   /* fetchAPII() {
-        const url = `https://api.openaq.org/v1/latest?country=${country.selectCountry}&limit=1000&parameter=pm25`;
-
-        fetch(url).then((response) => {
-            if (response.status === 200) {
-                return response.json()
-            } else {
-                throw new Error('Unable to fetch the data');
-            }
-        }).then((data) => {
-            let cities = data.results;
-            cities.sort((a, b) => (a.measurements[0].value > b.measurements[0].value) ? -1 : 1);
-            return cities;    
-        }).then((cities) => {
-            for(let i=0; i<10; i++){
-                    console.log(`${cities[i].city} - ${cities[i].measurements[0].parameter} - ${cities[i].measurements[0].value}`);
-                    pmList.push(cities[i].city);
-            }
-            return pmList;
-        }).then((pmList) => {
-
-            console.log(pmList.body);
-        }).catch((error) => {
-            console.log(`gagagag + ${error}`);
-        });
-               
-    }; */    
 };
