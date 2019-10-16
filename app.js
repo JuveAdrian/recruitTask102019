161fdad1cd39c77f99a80c0f930704835455be11
Adrian Grabkowski <adrian.grabkowski@wp.pl>
2019-10-16 23:15:17 +0200

    display all data, with bugs

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const nodeGeocoder = require('node-geocoder');
 
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -8,6 +9,14 @@ const errorRoutes = require('./routes/error');
 
 const app = express();
 
+const option = {
+    provider: 'google',
+    apiKey: 'AIzaSyDzaF4KiyKOfmQcOwwy5iBNfkBG-2rKI1M'
+}
+
+const geocoder = nodeGeocoder(option);
+
+
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
@@ -15,8 +24,23 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.json());
 
+
+app.use('/geotest', (req, res, next) => {
+
+    geocoder.reverse({lat: 43.306656, lon: -5.689716}, function(err, data) {
+        console.log(data[0].city);
+      });
+
+      geocoder.reverse({lat: 43.4015, lon: -5.80287}, function(err, data) {
+        console.log(data[0].city);
+      });
+      
+});
+
 app.use(userRoutes);
 app.use(errorRoutes);
+
+
 
 const port = process.env.PORT || 5656;
 
--- controllers/users.js
+++ controllers/users.js
@@ -1,5 +1,13 @@
 const Country = require('../models/country');
 const fetch = require('node-fetch');
+const nodeGeocoder = require('node-geocoder');
+
+const option = {
+    provider: 'google',
+    apiKey: 'AIzaSyDzaF4KiyKOfmQcOwwy5iBNfkBG-2rKI1M'
+}
+
+const geocoder = nodeGeocoder(option);
 
 exports.getIndexPage = (req, res, next) => {
     res.render('index', {
@@ -11,104 +19,99 @@ exports.postFormData = (req, res, next) => {
     const country = new Country(req.body.selectCountry);
     country.save();
     const data = country.fetchAll();
-
+    const citiesArr = [];
+    const citiesName = [];
     const url = `https://api.openaq.org/v1/latest?country=${data.selectCountry}&limit=3000&parameter=pm25`;
     console.log(url);
-
+    let names = [];
     fetch(url).then((response) => {
         if (response.status === 200) {
             return response.json()
         } else {
             throw new Error('Unable to fetch the data');
         }
-    }).then((data) => {
-        let cities = data.results;
-        const citiesArr = [];
-        cities.sort((a, b) => (a.measurements[0].value > b.measurements[0].value) ? -1 : 1);
-
-        let urlWikiApi = `https://pl.wikipedia.org/w/api.php?action=opensearch&format=json&search=`;
-        
-        for(let item of cities){
-            urlWikiApi = `https://pl.wikipedia.org/w/api.php?action=opensearch&format=json&search=${item.city}`;
-            const fetchdate = fetch(urlWikiApi).then((response) => {
-                if (response.status === 200) {
-                    return response.json();
-                } else {
-                    throw new Error('Unable to fetch the data');
-                }
-            }).then((data) => {
-                //console.log(data);
-                let temp = data[2][0]
-               return ( temp );
-                
-            }).catch((err) => {
-                if(err) {
-                    console.log('Blad pobierania opisu');
-                }
-            });
+        }).then((data) => {
+            const cities = data.results;
             
-            citiesArr.push({ name: item.city, value: item.measurements[0].value, desc: fetchdate} );
-        }
+            cities.sort((a, b) => (a.measurements[0].value > b.measurements[0].value) ? -1 : 1);
+            
+            //console.log(cities);
 
-        console.log(citiesArr);
-        
-        res.render('results', {
-            status: 'udalosiewyswietlic',
-            country: data.selectCountry,
-            cities: citiesArr
-        });
-        
-        return cities;
-    }).then((apidata) => {
-        
-            // ----------------------
-            // usunac duplikaty miast
-            // ----------------------
-        
-
-        for(let i=0; i<11; i++) {
-
-            let city_query;
-
-            if (country.selectCountry === "DE" || country.selectCountry == "ES") {
-                city_query = apidata[i].location;
-            } else {
-                city_query = apidata[i].city;
+            let urlWikiApi = `https://pl.wikipedia.org/w/api.php?action=opensearch&format=json&search=`;
+            
+            for(let item of cities){            
+                citiesArr.push({ name: item.city, coordinates: item.coordinates, value: item.measurements[0].value } );
             }
-            
-            city_query.replace(" ", "_");
-            
+           //console.log(citiesArr);
 
-            const urlWikiApi = `https://pl.wikipedia.org/w/api.php?action=opensearch&format=json&search=${city_query}`;
-            console.log(urlWikiApi);
-            fetch(urlWikiApi).then((response) => {
-                if (response.status === 200) {
-                    return response.json()
-                } else {
-                    throw new Error('Unable to fetch the data');
-                }
-            }).then((data, err) => {
-                if(err) {
-                    console.log(err);
-                } else {
-                    console.log(`OPIS - ${data[2][0]}`);
-                }
-                
-            }).catch((err) => {
-                if(err) {
-                    console.log('Blad pobierania opisu');
-                }
-            });
-            
-        }
-       
-       /*
-        res.render('results', {
-            status: 'udalosiewyswietlic',
-            country: data.selectCountry,
-            data: apidata,
-            country: country
+            let tabelaMiast = [];
+            for(let i=0; i<12; i++){
+                geocoder.reverse({lat: citiesArr[i].coordinates.latitude, lon: citiesArr[i].coordinates.longitude}, function(err, data) {
+                    if(citiesArr){
+                        fetch(`${urlWikiApi} + ${data[0].city}`).then((response) => {
+                            if (response.status === 200) {
+                                return response.json();
+                            } else {
+                                throw new Error('Unable to fetch the data');
+                            }
+                        }).then((datawiki) => {
+                            tabelaMiast.push({name: data[0].city, desc: datawiki[2][0]});
+                        });
+                        
+                    } else {
+                        console.log('brak wspolrzednych');
+                    }
+                });
+            }
+            function myFunction() {
+                setTimeout(function(){
+                      console.log(tabelaMiast) 
+
+                      console.log(`1 - ${tabelaMiast[0].name}`)
+                      res.render('results', {
+                        status: 'udalosiewyswietlic',
+                        cities: tabelaMiast
+                    });
+                    }, 1000);
+              }
+
+            myFunction(); 
+            /* res.render('results', {
+                status: 'udalosiewyswietlic',
+                cities: names
+            }); */
+
         });
-        */
-    });
+
+    
 };
+
+/* 
+res.render('results', {
+    status: 'udalosiewyswietlic',
+    country: data.selectCountry,
+    cities: citiesArr
+});
+ */
+
+
+/* let urlWikiApi = `https://pl.wikipedia.org/w/api.php?action=opensearch&format=json&search=`;
+        
+for(let item of cities){
+    urlWikiApi = `https://pl.wikipedia.org/w/api.php?action=opensearch&format=json&search=${item.city}`;
+    const fetchdate = fetch(urlWikiApi).then((response) => {
+        if (response.status === 200) {
+            return response.json();
+        } else {
+            throw new Error('Unable to fetch the data');
+        }
+    }).then((data) => {
+        //console.log(data);
+        let temp = data[2][0]
+       return ( temp );
+        
+    }).catch((err) => {
+        if(err) {
+            console.log('Blad pobierania opisu');
+        }
+    }); */
